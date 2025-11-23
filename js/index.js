c4b9be22f04825c6487bf4102d376657935523a1
Viktor <vikabr0524@gmail.com>
2025-11-23 13:25:07 +0100

    aside buttons, restructure

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const role = "admin"; // Possible roles: admin, responder, reporter, viewAll
+
 const login = function () {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -10,4 +12,16 @@ const login = function () {
 
 const signout = function () {
   window.location.href = "index.html";
+};
+
+const asideButtons = function () {
+  if (role === "viewAll") {
+    return;
+  }
+  const asideDivs = document.querySelectorAll(".aside-button-container");
+  asideDivs.forEach((div) => {
+    if (div.id !== role) {
+      div.style.display = "none";
+    }
+  });
 };
